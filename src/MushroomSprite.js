c43aa05298ef7ff3b3b1d04bbc734b78849efdb1
曹磊 <jumpcoder@gmail.com>
2013-07-16 20:52:52 +0800

    first commit

---
+++ src/MushroomSprite.js
@@ -0,0 +1,33 @@
+﻿
+var MushroomSprite = cc.Sprite.extend({
+	
+	ctor:function(){
+		this._super();
+		//赋予图片
+		this.initWithFile(s_mushroom);
+		this.radius = 40;
+		cc.Director.getInstance().getTouchDispatcher().addTargetedDelegate(this, 0, true);
+	},
+	containsTouchLocation:function(touch){
+		//获取触摸点位置
+		var getPoint = touch.getLocation();
+		//获取图片区域尺寸
+		var contentSize = this.getContentSize();
+		//定义拖拽的区域
+		var myRect = cc.rect(0,0,contentSize.width,contentSize.height);
+		myRect.origin.x += this.getPosition().x - this.getContentSize().width/2;
+		myRect.origin.y += this.getPosition().y - this.getContentSize().height/2;
+		
+		//判断点击是否在区域上
+		return cc.rectContainsPoint(myRect, getPoint);
+	},
+	onTouchBegan:function(touch, event){
+		if(!this.containsTouchLocation(touch)) return false;//判断触摸点是否在蘑菇上
+		return true;
+	},
+	onTouchMoved:function(touch,event){
+		cc.log('onTouchMoved');
+		var touchPoint = touch.getLocation();
+		this.setPositionX(touchPoint.x);//设置x轴位置等于触摸的x位置
+	}
+});
---
+++ src/resource.js
@@ -0,0 +1,47 @@
+
+var s_forest1 = 'forest1.jpg'; //±³¾°Í¼Æ¬Â·¾¶
+var s_mushroom = 'mushroom.png'; //Ä¢¹½Õý³£×´Ì¬
+var s_mushroom2 = 'mushroom2.png'; //Ä¢¹½±»Åö×²
+var s_bear_eyesopen = 'bear_eyesopen.png';
+var s_bear_eyesclose = 'bear_eyesclosed.png';
+var s_leaf = 'leaf.png';
+var s_flower = 'flower.png';
+var s_acorn = 'acorn.png';
+var s_start_button = 'start_button.png';
+var s_lives0 = 'lives0.png';
+var s_lives1 = 'lives1.png';
+var s_lives2 = 'lives2.png';
+var s_lives3 = 'lives3.png';
+var s_lives4 = 'lives4.png';
+var s_lives5 = 'lives5.png';
+var s_score = 'score.png';
+
+var g_ressources = [
+    //image
+    {src:s_forest1},
+    {src:s_mushroom},
+    {src:s_mushroom2},
+	{src:s_bear_eyesopen},
+	{src:s_bear_eyesclose},
+	{src:s_leaf},
+	{src:s_flower},
+	{src:s_acorn},
+	{src:s_start_button},
+	{src:s_lives0},
+	{src:s_lives1},
+	{src:s_lives2},
+	{src:s_lives3},
+	{src:s_lives4},
+	{src:s_lives5},
+	{src:s_score}
+
+    //plist
+
+    //fnt
+
+    //tmx
+
+    //bgm
+
+    //effect
+];
